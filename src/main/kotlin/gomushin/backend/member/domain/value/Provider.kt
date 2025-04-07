bcac371b53806b27eac1203f7ed09aaddeb68154
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-08 02:00:17 +0900

    refactor: OAuth2 프로바이더 이넘 value 추가

--- src/main/kotlin/gomushin/backend/member/domain/value/Provider.kt
+++ src/main/kotlin/gomushin/backend/member/domain/value/Provider.kt
@@ -1,7 +1,18 @@
 package gomushin.backend.member.domain.value
 
-enum class Provider {
-    GOOGLE,
-    KAKAO,
-    NAVER
+import gomushin.backend.core.infrastructure.exception.BadRequestException
+
+enum class Provider(
+    private val value: String
+) {
+    GOOGLE("google"),
+    KAKAO("kakao"),
+    NAVER("naver");
+
+    companion object {
+        fun getProviderByValue(value: String): Provider {
+            return entries.firstOrNull { it.value == value }
+                ?: throw BadRequestException("sarangggun.oauth.invalid-provider")
+        }
+    }
 }
