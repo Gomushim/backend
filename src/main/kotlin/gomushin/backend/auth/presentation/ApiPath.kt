203566e9388837b57476d01746842ebd55724185
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-08 01:58:49 +0900

    refactor: ApiPath에 기존 OAuth 제거

--- src/main/kotlin/gomushin/backend/auth/presentation/ApiPath.kt
+++ src/main/kotlin/gomushin/backend/auth/presentation/ApiPath.kt
@@ -1,8 +1,4 @@
 package gomushin.backend.auth.presentation
 
 object ApiPath {
-    object OAuth {
-        const val AUTHORIZE_OAUTH = "/v1/oauth/{provider}"
-        const val LOGIN_OAUTH = "/v1/oauth/login/{provider}"
-    }
 }
