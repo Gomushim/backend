6ec6379b97f300f080d802c7b3bbe7c4f585d50b
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-08 01:57:31 +0900

    refactor: 기존 OAuth2 로직 삭제

--- src/main/kotlin/gomushin/backend/auth/domain/oauth/OAuthProvider.kt
+++
@@ -1,7 +0,0 @@
-package gomushin.backend.auth.domain.oauth
-
-interface OAuthProvider {
-    fun getAuthorizationUrl(): String
-    fun getToken(code: String): OAuthToken
-    fun getUserInfo(accessToken: String): OAuthUserInfo
-}
--- src/main/kotlin/gomushin/backend/auth/domain/oauth/OAuthUserInfo.kt
+++
@@ -1,8 +0,0 @@
-package gomushin.backend.auth.domain.oauth
-
-data class OAuthUserInfo(
-    val id: String,
-    val email: String?,
-    val name: String?,
-    val profileImageUrl: String?
-)
