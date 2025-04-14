0854993ded386799aff40db20580cb3e25687db8
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-15 00:29:31 +0900

    refactor: Member에 role 추가

--- src/main/kotlin/gomushin/backend/member/domain/repository/MemberRepository.kt
+++ src/main/kotlin/gomushin/backend/member/domain/repository/MemberRepository.kt
@@ -4,6 +4,5 @@ import gomushin.backend.member.domain.entity.Member
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MemberRepository : JpaRepository<Member, Long> {
-    fun findByName(name: String): Member?
     fun findByEmail(email: String): Member?
 }
