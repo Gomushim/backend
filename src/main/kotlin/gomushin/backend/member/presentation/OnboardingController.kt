387356fa11c7e933fb1feca0c0a80b139d06176b
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-15 00:31:37 +0900

    feat: 온보딩 구현

---
+++ src/main/kotlin/gomushin/backend/member/presentation/OnboardingController.kt
@@ -0,0 +1,30 @@
+package gomushin.backend.member.presentation
+
+import gomushin.backend.core.CustomUserDetails
+import gomushin.backend.core.common.web.response.ApiResponse
+import gomushin.backend.member.application.OnboardingFacade
+import gomushin.backend.member.presentation.dto.request.OnboardingRequest
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@Tag(name = "온보딩", description = "OnboardingController")
+class OnboardingController(
+    private val onboardingFacade: OnboardingFacade,
+) {
+
+
+    @PostMapping(ApiPath.ONBOARDING)
+    @Operation(summary = "온보딩", description = "onboarding")
+    fun onboarding(
+        @AuthenticationPrincipal customUserDetails: CustomUserDetails,
+        @RequestBody onboardingRequest: OnboardingRequest
+    ): ApiResponse<Boolean> {
+        onboardingFacade.onboarding(customUserDetails.getId(), onboardingRequest)
+        return ApiResponse.success(true)
+    }
+}
