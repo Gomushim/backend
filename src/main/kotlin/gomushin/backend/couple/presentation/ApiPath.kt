297234388a90da2961e37367d439c94bbe8f07d4
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-16 22:31:37 +0900

    feat: 커플 코드 생성 및 커플 연결 URI

---
+++ src/main/kotlin/gomushin/backend/couple/presentation/ApiPath.kt
@@ -0,0 +1,6 @@
+package gomushin.backend.couple.presentation
+
+object ApiPath {
+    const val COUPLE_CODE_GENERATE = "/v1/couple/code-generate"
+    const val COUPLE_CONNECT = "/v1/couple/connect"
+}
