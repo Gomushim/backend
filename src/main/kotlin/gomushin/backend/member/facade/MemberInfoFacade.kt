a945525c0fc70eecfd3f4a497607e8bdb725ce61
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-18 14:21:00 +0900

    refactor: CRUD는 MemberService 로 묶기

--- src/main/kotlin/gomushin/backend/member/facade/MemberInfoFacade.kt
+++ src/main/kotlin/gomushin/backend/member/facade/MemberInfoFacade.kt
@@ -1,16 +1,16 @@
 package gomushin.backend.member.facade
 
 import gomushin.backend.core.CustomUserDetails
-import gomushin.backend.member.domain.service.MemberInfoService
+import gomushin.backend.member.domain.service.MemberService
 import gomushin.backend.member.dto.response.MyInfoResponse
 import org.springframework.stereotype.Component
 
 @Component
 class MemberInfoFacade(
-    private val memberInfoService: MemberInfoService,
+    private val memberService: MemberService,
 ) {
     fun getMemberInfo(customUserDetails: CustomUserDetails): MyInfoResponse {
-        val member = memberInfoService.getById(customUserDetails.getId())
+        val member = memberService.getById(customUserDetails.getId())
         return MyInfoResponse.of(member)
     }
 }
