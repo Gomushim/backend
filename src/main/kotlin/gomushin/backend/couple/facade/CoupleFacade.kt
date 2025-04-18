ed191ca73b758a7a55384aebb3bf2200d73afb99
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-18 14:22:17 +0900

    feat: 커플 연결 구현

--- src/main/kotlin/gomushin/backend/couple/facade/CoupleFacade.kt
+++ src/main/kotlin/gomushin/backend/couple/facade/CoupleFacade.kt
@@ -2,6 +2,7 @@ package gomushin.backend.couple.facade
 
 import gomushin.backend.core.CustomUserDetails
 import gomushin.backend.couple.domain.service.CoupleConnectService
+import gomushin.backend.couple.dto.request.CoupleAnniversaryRequest
 import gomushin.backend.couple.dto.request.CoupleConnectRequest
 import org.springframework.stereotype.Component
 
@@ -13,9 +14,19 @@ class CoupleFacade(
     fun requestCoupleCodeGeneration(customUserDetails: CustomUserDetails) =
         coupleConnectService.generateCoupleCode(customUserDetails.getId())
 
-
     fun requestCoupleConnect(
         customUserDetails: CustomUserDetails,
         request: CoupleConnectRequest
     ) = coupleConnectService.connectCouple(customUserDetails.getId(), request.coupleCode)
+
+    fun registerAnniversary(
+        customUserDetails: CustomUserDetails,
+        request: CoupleAnniversaryRequest
+    ) = coupleConnectService.registerAnniversary(
+        customUserDetails.getId(),
+        request.coupleId,
+        request.relationshipStartDate,
+        request.militaryStartDate,
+        request.militaryEndDate
+    )
 }
