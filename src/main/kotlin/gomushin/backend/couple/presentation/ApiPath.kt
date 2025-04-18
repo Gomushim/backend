b72089462d0c8050e07a93c4998cd84dfc345851
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-18 14:21:12 +0900

    feat: 커플 연결 API 생성

--- src/main/kotlin/gomushin/backend/couple/presentation/ApiPath.kt
+++ src/main/kotlin/gomushin/backend/couple/presentation/ApiPath.kt
@@ -3,4 +3,5 @@ package gomushin.backend.couple.presentation
 object ApiPath {
     const val COUPLE_CODE_GENERATE = "/v1/couple/code-generate"
     const val COUPLE_CONNECT = "/v1/couple/connect"
+    const val COUPLE_ANNIVERSARY = "/v1/couple/anniversary"
 }
