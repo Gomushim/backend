471b7314e4c32f52101863b55f2d23652b86cfd7
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-22 13:04:17 +0900

    feat: 온보딩 시 알림 받을 지 여부 설정

--- src/main/kotlin/gomushin/backend/member/facade/OnboardingFacade.kt
+++ src/main/kotlin/gomushin/backend/member/facade/OnboardingFacade.kt
@@ -1,5 +1,6 @@
 package gomushin.backend.member.facade
 
+import gomushin.backend.member.domain.service.NotificationService
 import gomushin.backend.member.domain.service.OnboardingService
 import gomushin.backend.member.dto.request.OnboardingRequest
 import org.springframework.stereotype.Component
@@ -7,7 +8,11 @@ import org.springframework.stereotype.Component
 @Component
 class OnboardingFacade(
     private val onboardingService: OnboardingService,
+    private val notificationService: NotificationService,
 ) {
 
-    fun onboarding(id: Long, onboardingRequest: OnboardingRequest) = onboardingService.onboarding(id, onboardingRequest)
+    fun onboarding(id: Long, onboardingRequest: OnboardingRequest) {
+        onboardingService.onboarding(id, onboardingRequest)
+        notificationService.initNotification(id, onboardingRequest.isNotification)
+    }
 }
