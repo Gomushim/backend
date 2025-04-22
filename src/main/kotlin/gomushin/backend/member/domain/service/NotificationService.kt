5fed3a65a4c9c3097c5f5a89acde991623021b6e
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-22 13:03:53 +0900

    feat: 온보딩 시 notification 생성 구현

---
+++ src/main/kotlin/gomushin/backend/member/domain/service/NotificationService.kt
@@ -0,0 +1,38 @@
+package gomushin.backend.member.domain.service
+
+import gomushin.backend.core.infrastructure.exception.BadRequestException
+import gomushin.backend.member.domain.entity.Notification
+import gomushin.backend.member.domain.repository.NotificationRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class NotificationService(
+    private val notificationRepository: NotificationRepository
+) {
+
+    @Transactional
+    fun initNotification(memberId: Long, isNotification: Boolean) {
+        val notification = Notification.create(memberId)
+        if (isNotification) {
+            notification.updateDday()
+            notification.updatePartnerStatus()
+        }
+        save(notification)
+    }
+
+    @Transactional(readOnly = true)
+    fun getByMemberId(memberId: Long): Notification {
+        return findByMemberId(memberId) ?: throw BadRequestException("sarangggun.member.not-exist-member")
+    }
+
+    @Transactional(readOnly = true)
+    fun findByMemberId(memberId: Long): Notification? {
+        return notificationRepository.findByMemberId(memberId)
+    }
+
+    @Transactional
+    fun save(notification: Notification): Notification {
+        return notificationRepository.save(notification)
+    }
+}
