fc821a33a1e0cb8969d6807e7250780a396d8bce
김영록 <praoo800@konkuk.ac.kr>
2025-04-23 22:20:12 +0900

    test : 이모지/상태 메시지 저장 api 구현 테스트 코드 작성 #22

--- src/test/kotlin/gomushin/backend/member/domain/service/MemberServiceTest.kt
+++ src/test/kotlin/gomushin/backend/member/domain/service/MemberServiceTest.kt
@@ -4,6 +4,7 @@ import gomushin.backend.member.domain.entity.Member
 import gomushin.backend.member.domain.repository.MemberRepository
 import gomushin.backend.member.domain.value.Provider
 import gomushin.backend.member.domain.value.Role
+import gomushin.backend.member.dto.request.UpdateMyEmotionAndStatusMessageRequest
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -45,5 +46,31 @@ class MemberServiceTest {
 
         // then
         assertEquals(expectedMember, result)
+    }
+
+    @DisplayName("이모지 및 상태 메시지 업데이트 - 성공")
+    @Test
+    fun updateMyEmotionAndStatusMessage() {
+        // given
+        val memberId = 1L
+        val expectedMember = Member(
+            id = 1L,
+            name = "테스트",
+            nickname = "테스트 닉네임",
+            email = "test@test.com",
+            birthDate = null,
+            profileImageUrl = null,
+            provider = Provider.KAKAO,
+            role = Role.GUEST,
+            emotion = 1,
+            statusMessage = "상태 변경전"
+        )
+        val updateMyEmotionAndStatusMessageRequest = UpdateMyEmotionAndStatusMessageRequest(2, "상태 변경후")
+        //when
+        `when`(memberRepository.findById(memberId)).thenReturn(Optional.of(expectedMember))
+        val result = memberService.updateMyEmotionAndStatusMessage(memberId, updateMyEmotionAndStatusMessageRequest)
+        //then
+        assertEquals(expectedMember.emotion, updateMyEmotionAndStatusMessageRequest.emotion)
+        assertEquals(expectedMember.statusMessage, updateMyEmotionAndStatusMessageRequest.statusMessage)
     }
 }
