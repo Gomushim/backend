9fcdbc4d573ab0be581b2308a577ee0058d6ab58
김영록 <praoo800@konkuk.ac.kr>
2025-04-24 00:31:30 +0900

    fix : main브랜치와의 rebase 충돌 해결 #22

--- src/main/kotlin/gomushin/backend/member/domain/service/MemberService.kt
+++ src/main/kotlin/gomushin/backend/member/domain/service/MemberService.kt
@@ -3,6 +3,7 @@ package gomushin.backend.member.domain.service
 import gomushin.backend.core.infrastructure.exception.BadRequestException
 import gomushin.backend.member.domain.entity.Member
 import gomushin.backend.member.domain.repository.MemberRepository
+import gomushin.backend.member.dto.request.UpdateMyEmotionAndStatusMessageRequest
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -20,5 +21,12 @@ class MemberService(
     @Transactional(readOnly = true)
     fun findById(id: Long): Member? {
         return memberRepository.findByIdOrNull(id)
+    }
+
+    @Transactional
+    fun updateMyEmotionAndStatusMessage(id: Long, updateMyEmotionAndStatusMessageRequest: UpdateMyEmotionAndStatusMessageRequest) {
+        val member = getById(id)
+        member.updateEmotion(updateMyEmotionAndStatusMessageRequest.emotion)
+        member.updateStatusMessage(updateMyEmotionAndStatusMessageRequest.statusMessage)
     }
 }
