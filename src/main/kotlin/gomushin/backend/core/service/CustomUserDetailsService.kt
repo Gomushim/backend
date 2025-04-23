f1c1041dadbacac5956ed809a39d633d79d1e9be
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-24 02:17:54 +0900

    feat: CustomUserDetails 에 getCouple 추가

--- src/main/kotlin/gomushin/backend/core/service/CustomUserDetailsService.kt
+++ src/main/kotlin/gomushin/backend/core/service/CustomUserDetailsService.kt
@@ -2,21 +2,25 @@ package gomushin.backend.core.service
 
 import gomushin.backend.core.CustomUserDetails
 import gomushin.backend.core.infrastructure.exception.BadRequestException
+import gomushin.backend.couple.domain.repository.CoupleRepository
 import gomushin.backend.member.domain.repository.MemberRepository
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
-class CustomUserDetailsService(private val memberRepository: MemberRepository) : UserDetailsService {
-    override fun loadUserByUsername(email: String): UserDetails {  // 파라미터명 username → email로 변경
+class CustomUserDetailsService(
+    private val memberRepository: MemberRepository,
+    private val coupleRepository: CoupleRepository,
+) : UserDetailsService {
+    override fun loadUserByUsername(email: String): UserDetails {
         val member = memberRepository.findByEmail(email)
             ?: throw BadRequestException("sarangggun.member.not-exist-member")
-        return CustomUserDetails(member)
+        return CustomUserDetails(member, coupleRepository)
     }
 
     fun loadUserById(id: Long): UserDetails =
         memberRepository.findById(id)
-            .map { CustomUserDetails(it) }
+            .map { CustomUserDetails(it, coupleRepository) }
             .orElseThrow { BadRequestException("sarangggun.member.not-exist-member") }
 }
