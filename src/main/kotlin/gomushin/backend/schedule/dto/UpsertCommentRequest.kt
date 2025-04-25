05777978840fdf5575a0b12171e91a9c3f71bbee
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-25 13:45:55 +0900

    feat: 댓글 도메인 CUD 비즈니스 로직 구현

---
+++ src/main/kotlin/gomushin/backend/schedule/dto/UpsertCommentRequest.kt
@@ -0,0 +1,10 @@
+package gomushin.backend.schedule.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class UpsertCommentRequest(
+    @Schema(description = "댓글 ID(새로 생성 시 null, 업데이트 시 id)", example = "1")
+    val commentId: Long?,
+    @Schema(description = "내용", example = "훈련 힘내")
+    val content: String,
+)
