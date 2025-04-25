2943933ec03f6b9ee3921f67395bea5f1492241b
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-25 13:43:46 +0900

    feat: API 추가

--- src/main/kotlin/gomushin/backend/schedule/presentation/ApiPath.kt
+++ src/main/kotlin/gomushin/backend/schedule/presentation/ApiPath.kt
@@ -3,4 +3,10 @@ package gomushin.backend.schedule.presentation
 object ApiPath {
     const val SCHEDULES = "/v1/schedules"
     const val SCHEDULE = "/v1/schedules/{scheduleId}"
+
+    const val LETTERS = "/v1/schedules/letters"
+    const val LETTER = "/v1/schedules/{scheduleId}/letters/{letterId}"
+
+    const val COMMENTS = "/v1/schedules/letters/{letterId}/comments"
+    const val COMMENT = "/v1/schedules/letters/{letterId}/comments/{commentId}"
 }
