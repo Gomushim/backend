139a9c3b8bcfdbe96481725bb006bc3485dd605f
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-25 17:26:36 +0900

    refactor: s3 물리적 삭제

--- src/main/kotlin/gomushin/backend/schedule/domain/repository/PictureRepository.kt
+++ src/main/kotlin/gomushin/backend/schedule/domain/repository/PictureRepository.kt
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface PictureRepository: JpaRepository<Picture, Long> {
     fun findAllByPictureUrlIn(urls: List<String>): List<Picture>
     fun deleteAllByLetterId(letterId: Long)
+    fun findAllByLetterId(letterId: Long): List<Picture>
 }
