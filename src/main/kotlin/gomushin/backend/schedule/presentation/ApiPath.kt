85b94b16bdc96678ebecf31d13494b31d825683e
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-27 01:00:08 +0900

    feat: 캘린더 일정 조회 컨트롤러 구현

--- src/main/kotlin/gomushin/backend/schedule/presentation/ApiPath.kt
+++ src/main/kotlin/gomushin/backend/schedule/presentation/ApiPath.kt
@@ -3,6 +3,7 @@ package gomushin.backend.schedule.presentation
 object ApiPath {
     const val SCHEDULES = "/v1/schedules"
     const val SCHEDULE = "/v1/schedules/{scheduleId}"
+    const val SCHEDULES_BY_DATE = "/v1/schedules/date"
 
     const val LETTERS = "/v1/schedules/letters"
     const val LETTER = "/v1/schedules/{scheduleId}/letters/{letterId}"
