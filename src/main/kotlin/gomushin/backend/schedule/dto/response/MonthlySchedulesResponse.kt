74ca111824c3a4be006d0e1cf5343d4e04f08a88
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-27 01:00:33 +0900

    feat: 캘린더 일정 조회 응답 Response 구현

---
+++ src/main/kotlin/gomushin/backend/schedule/dto/response/MonthlySchedulesResponse.kt
@@ -0,0 +1,10 @@
+package gomushin.backend.schedule.dto.response
+
+import java.time.LocalDateTime
+
+data class MonthlySchedulesResponse(
+    val content: String,
+    val startDate: LocalDateTime,
+    val endDate: LocalDateTime,
+    val fatigue: String,
+)
