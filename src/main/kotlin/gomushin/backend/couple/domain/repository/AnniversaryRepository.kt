1b26f4af1e8a6bb8fa062b0f05614d09a6f4021e
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-27 23:52:16 +0900

    refactor: 월 별 일정 응답 시 기념일 제외, 일 별 일정 응답 시 기념일 추가

--- src/main/kotlin/gomushin/backend/couple/domain/repository/AnniversaryRepository.kt
+++ src/main/kotlin/gomushin/backend/couple/domain/repository/AnniversaryRepository.kt
@@ -2,10 +2,12 @@ package gomushin.backend.couple.domain.repository
 
 import gomushin.backend.couple.domain.entity.Anniversary
 import gomushin.backend.couple.dto.response.MonthlyAnniversariesResponse
+import gomushin.backend.schedule.dto.response.DailyAnniversaryResponse
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
+import java.time.LocalDate
 
 interface AnniversaryRepository : JpaRepository<Anniversary, Long> {
     @Modifying
@@ -15,8 +17,9 @@ interface AnniversaryRepository : JpaRepository<Anniversary, Long> {
     WHERE (a.title LIKE '%일' OR a.title LIKE '%주년')
     AND a.anniversaryProperty = 0
     And a.coupleId = :coupleId
-""")
-    fun deleteAnniversariesWithTitleEndingAndPropertyZero(@Param("coupleId")coupleId : Long)
+"""
+    )
+    fun deleteAnniversariesWithTitleEndingAndPropertyZero(@Param("coupleId") coupleId: Long)
 
     @Modifying
     @Query("DELETE FROM Anniversary a WHERE a.coupleId = :coupleId")
@@ -35,4 +38,19 @@ interface AnniversaryRepository : JpaRepository<Anniversary, Long> {
     """
     )
     fun findByCoupleIdAndYearAndMonth(coupleId: Long, year: Int, month: Int): List<MonthlyAnniversariesResponse>
+
+    @Query(
+        """
+        SELECT new gomushin.backend.schedule.dto.response.DailyAnniversaryResponse(
+            a.id, 
+            a.title, 
+            a.anniversaryDate
+        )
+        FROM Anniversary a
+        WHERE a.coupleId = :coupleId 
+        AND function('DATE', a.anniversaryDate) = :startDate
+    """
+    )
+    fun findByCoupleIdAndDate(coupleId: Long, date: LocalDate): List<DailyAnniversaryResponse>
+
 }
--- src/main/kotlin/gomushin/backend/schedule/dto/response/MonthlySchedulesAndAnniversariesResponse.kt
+++ src/main/kotlin/gomushin/backend/schedule/dto/response/MonthlySchedulesAndAnniversariesResponse.kt
@@ -4,14 +4,14 @@ import gomushin.backend.couple.dto.response.MonthlyAnniversariesResponse
 
 data class MonthlySchedulesAndAnniversariesResponse(
     val schedules: List<MonthlySchedulesResponse>,
-    val anniversaries: List<MonthlyAnniversariesResponse>,
+//    val anniversaries: List<MonthlyAnniversariesResponse>,
 ) {
     companion object {
         fun of(
             schedules: List<MonthlySchedulesResponse>,
             anniversaries: List<MonthlyAnniversariesResponse>,
         ): MonthlySchedulesAndAnniversariesResponse {
-            return MonthlySchedulesAndAnniversariesResponse(schedules, anniversaries)
+            return MonthlySchedulesAndAnniversariesResponse(schedules)
         }
     }
 }
