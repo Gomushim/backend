4765331cf2f905291dceda230639235eb05bfcc1
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-27 18:22:51 +0900

    feat: Picture 레포지토리 미리보기 사진 추가

--- src/main/kotlin/gomushin/backend/schedule/domain/repository/PictureRepository.kt
+++ src/main/kotlin/gomushin/backend/schedule/domain/repository/PictureRepository.kt
@@ -6,10 +6,11 @@ import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
-interface PictureRepository: JpaRepository<Picture, Long> {
+interface PictureRepository : JpaRepository<Picture, Long> {
     fun findAllByPictureUrlIn(urls: List<String>): List<Picture>
     fun deleteAllByLetterId(letterId: Long)
     fun findAllByLetterId(letterId: Long): List<Picture>
+    fun findFirstByLetterIdOrderByIdAsc(letterId: Long): Picture?
 
     @Query("SELECT p FROM Picture p WHERE p.letterId IN :letterIds")
     fun findAllByLetterIdIn(@Param("letterIds") letterIds: List<Long>): List<Picture>
