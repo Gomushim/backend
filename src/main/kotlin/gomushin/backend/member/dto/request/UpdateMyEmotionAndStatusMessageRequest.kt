1521c66f343fcb4db67aa6fef8b62ecf2a1f9c49
김영록 <praoo800@konkuk.ac.kr>
2025-04-29 21:47:13 +0900

    chore : swagger request 설명 수정(emotion타입이 바뀌어서 그거에 맞춰 예시 리퀘스트도 수정) #54

--- src/main/kotlin/gomushin/backend/member/dto/request/UpdateMyEmotionAndStatusMessageRequest.kt
+++ src/main/kotlin/gomushin/backend/member/dto/request/UpdateMyEmotionAndStatusMessageRequest.kt
@@ -9,7 +9,7 @@ import jakarta.persistence.Enumerated
 data class UpdateMyEmotionAndStatusMessageRequest(
     @Enumerated(EnumType.STRING)
     @Schema(description = "이모지(MISS : 보고싶어요, GOOD: 기분 좋아요, COMMON : 아무느낌 없어요, " +
-            "TIRED : 피곤해요, SAD: 서운해요, WORRY : 걱정돼요, ANGRY : 짜증나요)", example = "1")
+            "TIRED : 피곤해요, SAD: 서운해요, WORRY : 걱정돼요, ANGRY : 짜증나요)", example = "COMMON")
     val emotion : Emotion,
 
     @Schema(description = "상태 메시지", example = "보고 싶어요")
