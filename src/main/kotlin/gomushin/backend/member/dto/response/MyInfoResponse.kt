49e8c5c286c4627b983f555ab233b78e44aa424e
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-30 17:36:31 +0900

    refactor: 유저 정보 반환 시 커플 여부 , 온보딩 여부 반환

--- src/main/kotlin/gomushin/backend/member/dto/response/MyInfoResponse.kt
+++ src/main/kotlin/gomushin/backend/member/dto/response/MyInfoResponse.kt
@@ -1,15 +1,21 @@
 package gomushin.backend.member.dto.response
 
 import gomushin.backend.member.domain.entity.Member
+import io.swagger.v3.oas.annotations.media.Schema
 
 data class MyInfoResponse(
+    @Schema(description = "내 닉네임", example = "닉네임")
     val nickname: String,
+    @Schema(description = "커플 여부", example = "true")
     val isCouple: Boolean,
+    @Schema(description = "온보딩 여부 (GUEST : 온보딩 X , MEMBER : 온보딩 O)", example = "MEMBER")
+    val role: String
 ) {
     companion object {
         fun of(member: Member) = MyInfoResponse(
             member.nickname,
-            member.isCouple
+            member.isCouple,
+            member.role.name
         )
     }
 }
