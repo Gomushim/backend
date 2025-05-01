ae55a4288c977d0271e692a53c18b1f9e580b753
김영록 <praoo800@konkuk.ac.kr>
2025-05-02 02:40:23 +0900

    fix : 리뷰 반영(Logger객체 사용, 전송실패 시 잘못된 에러메시지 삽입된거 수정) #57

--- src/main/kotlin/gomushin/backend/alarm/service/QuestionAlarmService.kt
+++ src/main/kotlin/gomushin/backend/alarm/service/QuestionAlarmService.kt
@@ -7,7 +7,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
-import org.hibernate.query.sqm.tree.SqmNode.log
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -17,6 +18,7 @@ class QuestionAlarmService (
     private val fcmService: FCMService,
     private val memberService: MemberService
 ) {
+    private val log: Logger = LoggerFactory.getLogger(QuestionAlarmService::class.java)
     private val questionMessages = listOf(
         "오늘 하루는 어땠나요?+연인에게도 안부를 전해보세요",
         "사랑은 매일 채우는 것+오늘도 연인과 한 조각 채워보세요",
@@ -37,7 +39,7 @@ class QuestionAlarmService (
                         fcmService.sendMessageTo(member.fcmToken, title, sendContent)
                     } catch (e: Exception) {
                         log.error("질문형 메시지 전송오류 : 수신자 {${member.name}}, 전송시각{${LocalDateTime.now()}}\n")
-                        throw BadRequestException("sarangggun.alarm.fail-issue-accesstoken")
+                        throw BadRequestException("sarangggun.alarm.fail-send-alarm")
                     }
                 }
             }.awaitAll()
