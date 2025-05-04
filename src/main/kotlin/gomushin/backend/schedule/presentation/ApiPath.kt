37f5ae8cb0dfdc3579868e9d1093f5b9e7cb3adb
HoyeongJeon <hoyeong0302@gmail.com>
2025-05-04 15:02:09 +0900

    refactor: 안쓰는 url 제거

--- src/main/kotlin/gomushin/backend/schedule/presentation/ApiPath.kt
+++ src/main/kotlin/gomushin/backend/schedule/presentation/ApiPath.kt
@@ -9,7 +9,6 @@ object ApiPath {
 
     const val LETTERS = "/v1/schedules/letters"
     const val LETTER = "/v1/schedules/{scheduleId}/letters/{letterId}"
-    const val LETTERS_TO_ME = "/v1/schedules/letters/to-me"
     const val LETTERS_BY_SCHEDULE = "/v1/schedules/{scheduleId}"
     const val LETTERS_MAIN = "/v1/schedules/letters/main"
 
