8fbadb061ab7a2a868cb106d91fa2aa6c52d8cbf
김영록 <praoo800@konkuk.ac.kr>
2025-05-12 03:47:38 +0900

    refactor : 쿠키만드는 메서드 서비스 클래스로 따로 분리해서 관리(확장성 위함, reissueFacade에서도 쿠키 만드는게 필요함) #90

--- src/main/kotlin/gomushin/backend/core/oauth/handler/CustomSuccessHandler.kt
+++ src/main/kotlin/gomushin/backend/core/oauth/handler/CustomSuccessHandler.kt
@@ -1,5 +1,6 @@
 package gomushin.backend.core.oauth.handler
 
+import gomushin.backend.core.configuration.cookie.CookieService
 import gomushin.backend.core.configuration.redis.RedisService
 import gomushin.backend.core.infrastructure.exception.BadRequestException
 import gomushin.backend.core.jwt.infrastructure.TokenService
@@ -11,7 +12,6 @@ import jakarta.servlet.ServletException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.ResponseCookie
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
 import org.springframework.stereotype.Component
@@ -21,8 +21,8 @@ class CustomSuccessHandler(
     private val tokenService: TokenService,
     private val memberRepository: MemberRepository,
     private val redisService: RedisService,
+    private val cookieService: CookieService,
     @Value("\${redirect-url}") private val redirectUrl: String,
-    @Value("\${cookie.domain}") private val cookieDomain: String,
 ) : SimpleUrlAuthenticationSuccessHandler() {
 
     @Throws(IOException::class, ServletException::class)
@@ -45,22 +45,11 @@ class CustomSuccessHandler(
             accessToken = tokenService.provideAccessToken(principal.getUserId(), principal.getRole())
         }
 
-        val accessCookie = createCookie("access_token", accessToken)
-        val refreshCookie = createCookie("refresh_token", refreshToken)
+        val accessCookie = cookieService.createCookie("access_token", accessToken)
+        val refreshCookie = cookieService.createCookie("refresh_token", refreshToken)
         response!!.addHeader("Set-Cookie", accessCookie.toString())
         response.addHeader("Set-Cookie", refreshCookie.toString())
         response.sendRedirect(redirectUrl)
-    }
-
-    private fun createCookie(key: String, value: String): ResponseCookie {
-        return ResponseCookie.from(key, value)
-            .path("/")
-            .httpOnly(true)
-            .secure(true)
-            .sameSite("None")
-            .domain("localhost") //Todo push할때 cookie domain으로 바꾸기
-            .maxAge(432000)
-            .build()
     }
 
     private fun getMemberByEmail(email: String): Member? {
