dcd2c705fe4f7193b5ea922ca4349eeb225c5cef
HoyeongJeon <hoyeong0302@gmail.com>
2025-05-11 17:24:41 +0900

    fix: 페이지 기반 페이지네이션으로 응답 수정

--- src/main/kotlin/gomushin/backend/core/common/web/PageResponse.kt
+++ src/main/kotlin/gomushin/backend/core/common/web/PageResponse.kt
@@ -1,34 +1,23 @@
 package gomushin.backend.core.common.web
 
 import io.swagger.v3.oas.annotations.media.Schema
+import org.springframework.data.domain.Page
 
 data class PageResponse<T>(
     @Schema(description = "페이지 응답")
-    val data: List<T>?,
-    @Schema(description = "다음 페이지를 위한 커서 키")
-    val after: Long?,
-    @Schema(description = "데이터 수")
-    val count: Int,
-//    @Schema(description = "다음 페이지 URL")
-//    val next: String?,
+    val data: List<T>,
+    @Schema(description = "전체 페이지 수")
+    val totalPages: Int,
     @Schema(description = "마지막 페이지 여부")
     val isLastPage: Boolean,
 ) {
     companion object {
-        fun <T> of(
-            data: List<T>,
-            after: Long?,
-            count: Int,
-//            next: String?,
-            isLastPage: Boolean,
-        ): PageResponse<T> {
-            return PageResponse(
-                data = data,
-                after = after,
-                count = count,
-//                next = next,
-                isLastPage = isLastPage
-            )
-        }
+        fun <T> from(
+            page: Page<T>
+        ): PageResponse<T> = PageResponse(
+            data = page.content,
+            totalPages = page.totalPages,
+            isLastPage = page.isLast
+        )
     }
 }
