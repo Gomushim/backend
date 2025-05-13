ab08559b4ff5dbac5c1f303d5cdf41d4833f4920
HoyeongJeon <hoyeong0302@gmail.com>
2025-05-13 20:42:53 +0900

    feat: 로깅 추가

--- src/main/kotlin/gomushin/backend/core/oauth/handler/CustomAccessDeniedHandler.kt
+++ src/main/kotlin/gomushin/backend/core/oauth/handler/CustomAccessDeniedHandler.kt
@@ -5,6 +5,7 @@ import gomushin.backend.core.common.web.response.ExtendedHttpStatus
 import gomushin.backend.core.common.web.response.exception.ErrorCodeResolvingApiErrorException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.security.access.AccessDeniedException
 import org.springframework.security.web.access.AccessDeniedHandler
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component
 
 @Component
 class CustomAccessDeniedHandler(private val objectMapper: ObjectMapper) : AccessDeniedHandler {
+    private val logger = LoggerFactory.getLogger(CustomAccessDeniedHandler::class.java)
     override fun handle(
         request: HttpServletRequest,
         response: HttpServletResponse,
@@ -24,6 +26,8 @@ class CustomAccessDeniedHandler(private val objectMapper: ObjectMapper) : Access
         response.status = HttpServletResponse.SC_FORBIDDEN
         response.characterEncoding = "UTF-8"
 
+        // 정확히 어떤 이유로 발생한 에러인지 보려면 어떻게 해야하나
+        logger.error("Access Denied: ${accessDeniedException.message}")
 
         val errorCode = if (request.requestURI.contains("/v1/member/onboarding")) {
             "sarangggun.auth.guest-only"
