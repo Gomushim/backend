ffecc6a697b44fa8b9e47d592119e358c5df02f8
HoyeongJeon <hoyeong0302@gmail.com>
2025-05-16 18:00:41 +0900

    fix : 스케쥴 상세조회 하루종일 여부 추가

--- src/main/kotlin/gomushin/backend/schedule/dto/response/ScheduleDetailResponse.kt
+++ src/main/kotlin/gomushin/backend/schedule/dto/response/ScheduleDetailResponse.kt
@@ -4,22 +4,24 @@ import gomushin.backend.schedule.domain.entity.Schedule
 import java.time.LocalDateTime
 
 data class ScheduleDetailResponse(
-    val id : Long,
-    val title : String,
-    val fatigue : String,
-    val startDate : LocalDateTime,
-    val endDate : LocalDateTime,
-    val letters : List<LetterPreviewResponse>
+    val id: Long,
+    val title: String,
+    val fatigue: String,
+    val startDate: LocalDateTime,
+    val endDate: LocalDateTime,
+    val isAllDay: Boolean,
+    val letters: List<LetterPreviewResponse>,
 ) {
     companion object {
-        fun of(schedule : Schedule, letters: List<LetterPreviewResponse>) : ScheduleDetailResponse {
+        fun of(schedule: Schedule, letters: List<LetterPreviewResponse>): ScheduleDetailResponse {
             return ScheduleDetailResponse(
                 schedule.id,
                 schedule.title,
                 schedule.fatigue,
                 schedule.startDate,
                 schedule.endDate,
-                letters
+                schedule.isAllDay,
+                letters,
             )
         }
     }
