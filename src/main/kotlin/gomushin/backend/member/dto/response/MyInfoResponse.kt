65f522a8235f52390936e7c4b69da65c4b326bff
HoyeongJeon <hoyeong0302@gmail.com>
2025-05-24 15:36:13 +0900

    fix: my-info filter 제거

--- src/main/kotlin/gomushin/backend/member/dto/response/MyInfoResponse.kt
+++ src/main/kotlin/gomushin/backend/member/dto/response/MyInfoResponse.kt
@@ -5,11 +5,11 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 data class MyInfoResponse(
     @Schema(description = "내 닉네임", example = "닉네임")
-    val nickname: String,
+    val nickname: String?,
     @Schema(description = "커플 여부", example = "true")
-    val isCouple: Boolean,
+    val isCouple: Boolean?,
     @Schema(description = "온보딩 여부 (GUEST : 온보딩 X , MEMBER : 온보딩 O)", example = "MEMBER")
-    val role: String
+    val role: String?
 ) {
     companion object {
         fun of(member: Member) = MyInfoResponse(
