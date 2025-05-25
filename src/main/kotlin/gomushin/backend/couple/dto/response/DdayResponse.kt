cda9833238e0adeb3c02d1d62e86ae632fc0805d
김영록 <praoo800@konkuk.ac.kr>
2025-05-26 02:18:30 +0900

    fix : 디데이 정보 조회시 양수이면 +붙이고, 0이면 -DAY로 표기되게끔 수정 #140

--- src/main/kotlin/gomushin/backend/couple/dto/response/DdayResponse.kt
+++ src/main/kotlin/gomushin/backend/couple/dto/response/DdayResponse.kt
@@ -1,17 +1,24 @@
 package gomushin.backend.couple.dto.response
 
 data class DdayResponse (
-        val sinceLove : Int?,
-        val sinceMilitaryStart : Int?,
-        val militaryEndLeft : Int?
+        val sinceLove : String?,
+        val sinceMilitaryStart : String?,
+        val militaryEndLeft : String?
 ){
     companion object{
-        fun of(sinceLove: Int?,
-               sinceMilitaryStart: Int?,
-               militaryEndLeft: Int?) = DdayResponse (
-                sinceLove,
-                sinceMilitaryStart,
-                militaryEndLeft
-        )
+        fun of(
+            sinceLove: Int?,
+            sinceMilitaryStart: Int?,
+            militaryEndLeft: Int?
+        ): DdayResponse {
+            fun format(day: Int?): String? =
+                day?.let { if (it > 0) "+$it" else if (it == 0) "-DAY" else it.toString() }
+
+            return DdayResponse(
+                sinceLove = format(sinceLove),
+                sinceMilitaryStart = format(sinceMilitaryStart),
+                militaryEndLeft = format(militaryEndLeft)
+            )
+        }
     }
 }
