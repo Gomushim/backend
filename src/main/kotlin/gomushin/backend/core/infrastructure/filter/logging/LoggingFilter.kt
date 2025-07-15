982290b18def614f3032fb664b71c1de42c200c4
김영록 <praoo800@konkuk.ac.kr>
2025-07-16 01:52:11 +0900

    feat: 컨트롤러 로깅 AOP 구현 #153

---
+++ src/main/kotlin/gomushin/backend/core/infrastructure/filter/logging/LoggingFilter.kt
@@ -0,0 +1,45 @@
+package gomushin.backend.core.infrastructure.filter.logging
+
+import gomushin.backend.core.CustomUserDetails
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.LoggerFactory
+import org.springframework.security.authentication.AnonymousAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class LoggingFilter : Filter {
+
+    private val log = LoggerFactory.getLogger(LoggingFilter::class.java)
+
+    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+        if (request is HttpServletRequest) {
+            val wrappedRequest = CachedBodyHttpServletRequest(request)
+
+            val url = wrappedRequest.requestURI
+            val method = wrappedRequest.method
+            val body = runCatching { wrappedRequest.reader.readLines().joinToString("") }.getOrNull()
+
+            val authentication = SecurityContextHolder.getContext().authentication
+
+            val userId: Long? = when {
+                authentication == null || !authentication.isAuthenticated ||
+                        authentication is AnonymousAuthenticationToken -> null
+                authentication.principal is CustomUserDetails -> {
+                    (authentication.principal as CustomUserDetails).getId()
+                }
+                else -> null
+            }
+
+            log.info("[REQUEST LOG] userId={}, URL={}, Method={}, Body={}", userId, url, method, body)
+
+            chain.doFilter(wrappedRequest, response)
+        } else {
+            chain.doFilter(request, response)
+        }
+    }
+}
