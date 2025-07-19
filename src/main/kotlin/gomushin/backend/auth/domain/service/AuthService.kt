ad932739c4805df84b53bdff081f0354e4db96c1
김영록 <praoo800@konkuk.ac.kr>
2025-07-19 11:34:45 +0900

    hotfix : 삭제할 쿠키 도메인 명시, secure설정 true로 변경 #157

--- src/main/kotlin/gomushin/backend/auth/domain/service/AuthService.kt
+++ src/main/kotlin/gomushin/backend/auth/domain/service/AuthService.kt
@@ -2,10 +2,14 @@ package gomushin.backend.auth.domain.service
 
 import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class AuthService {
+class AuthService(
+    @Value("\${cookie.domain}")
+    private val cookieDomain: String
+) {
     companion object {
         private const val AT_PREFIX = "access_token"
         private const val RT_PREFIX = "refresh_token"
@@ -14,13 +18,17 @@ class AuthService {
     fun logout(response: HttpServletResponse) {
         val expiredAccess = Cookie(AT_PREFIX, "").apply {
             path = "/"
+            domain = cookieDomain
             isHttpOnly = true
+            secure = true
             maxAge = 0
         }
 
         val expiredRefresh = Cookie(RT_PREFIX, "").apply {
             path = "/"
+            domain = cookieDomain
             isHttpOnly = true
+            secure = true
             maxAge = 0
         }
 
