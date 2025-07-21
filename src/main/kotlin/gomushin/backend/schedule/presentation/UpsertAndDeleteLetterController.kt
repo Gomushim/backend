13bcbbf040240ea67a50c9100ba3d78192e8e7e1
김영록 <praoo800@konkuk.ac.kr>
2025-07-22 00:59:39 +0900

    fix : multipart일때는 controller에서 로깅을 수행 #163

--- src/main/kotlin/gomushin/backend/schedule/presentation/UpsertAndDeleteLetterController.kt
+++ src/main/kotlin/gomushin/backend/schedule/presentation/UpsertAndDeleteLetterController.kt
@@ -2,12 +2,13 @@ package gomushin.backend.schedule.presentation
 
 import gomushin.backend.core.CustomUserDetails
 import gomushin.backend.core.common.web.response.ApiResponse
+import gomushin.backend.core.infrastructure.filter.logging.LoggingFilter
 import gomushin.backend.schedule.dto.request.UpsertLetterRequest
 import gomushin.backend.schedule.facade.UpsertAndDeleteLetterFacade
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
@@ -17,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile
 class UpsertAndDeleteLetterController(
     private val upsertAndDeleteLetterFacade: UpsertAndDeleteLetterFacade,
 ) {
+    private val log = LoggerFactory.getLogger(UpsertAndDeleteLetterController::class.java)
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(
         ApiPath.LETTERS
@@ -24,9 +26,10 @@ class UpsertAndDeleteLetterController(
     @Operation(summary = "편지 수정하거나 추가하기", description = "upsertLetter")
     fun upsertLetter(
         @AuthenticationPrincipal customUserDetails: CustomUserDetails,
-        @RequestPart("upsertLetterRequest") upsertLetterRequest: UpsertLetterRequest,
+        @RequestPart upsertLetterRequest: UpsertLetterRequest,
         @RequestPart("pictures", required = false) pictures: List<MultipartFile>?,
     ): ApiResponse<Boolean> {
+        log.info("[REQUEST LOG] userId={}, URL={}, Method={}, Body={}", customUserDetails.getId(), ApiPath.LETTERS, "POST", upsertLetterRequest)
         upsertAndDeleteLetterFacade.upsert(customUserDetails, upsertLetterRequest, pictures)
         return ApiResponse.success(true)
     }
